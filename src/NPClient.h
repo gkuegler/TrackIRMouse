39a965b8c51520e7ea81bd8c248dc79a9c9a589f
George Kuegler <georgekuegler@gmail.com>
2021-06-25 23:33:14 -0400

    revert NPClient.h back to SDK original

--- src/NPClient.h
+++ src/NPClient.h
@@ -13,11 +13,11 @@
 // *
 // *******************************************************************************
 
-//#define DLL_NP_LOCATION "C:\\Program Files (x86)\\NaturalPoint\\TrackIR5"
-#ifndef TRACKIRMOUSE_NP_CLIENT_H
-#define TRACKIRMOUSE_NP_CLIENT_H
-
+// added by GK
 #include <Windows.h>
+
+#ifndef _NPCLIENT_H_DEFINED_
+#define _NPCLIENT_H_DEFINED_
 
 #pragma pack( push, npclient_h ) // Save current pack value
 #pragma pack(1)
@@ -60,14 +60,14 @@
 						// the second parameter of a message bearing control data information contains a packed data format. 
 						// The High byte indicates what the data is, and the Low byte contains the actual data
 // roll, pitch, yaw
-#define	NPRoll		1	// +/- 16383 (representing +/- 180) [data = input - 16383] float
-#define	NPPitch		2	// +/- 16383 (representing +/- 180) [data = input - 16383] float
-#define	NPYaw		4	// +/- 16383 (representing +/- 180) [data = input - 16383] float
+#define	NPRoll		1	// +/- 16383 (representing +/- 180) [data = input - 16383]
+#define	NPPitch		2	// +/- 16383 (representing +/- 180) [data = input - 16383]
+#define	NPYaw		4	// +/- 16383 (representing +/- 180) [data = input - 16383]
 
 // x, y, z - remaining 6dof coordinates
-#define	NPX			16	// +/- 16383 [data = input - 16383] float
-#define	NPY			32	// +/- 16383 [data = input - 16383] float
-#define	NPZ			64	// +/- 16383 [data = input - 16383] float
+#define	NPX			16	// +/- 16383 [data = input - 16383]
+#define	NPY			32	// +/- 16383 [data = input - 16383]
+#define	NPZ			64	// +/- 16383 [data = input - 16383]
 
 // raw object position from imager
 #define	NPRawX		128	// 0..25600 (actual value is multiplied x 100 to pass two decimal places of precision)  [data = input / 100]
@@ -91,25 +91,24 @@
 
 // NPESULT values are returned from the Game Client API functions.
 //
-//enum class E_MY_FAVOURITE_FRUITS : unsigned char
 typedef enum tagNPResult
 {
 	NP_OK = 0,
-    NP_ERR_DEVICE_NOT_PRESENT,
+	NP_ERR_DEVICE_NOT_PRESENT,
 	NP_ERR_UNSUPPORTED_OS,
 	NP_ERR_INVALID_ARG,
 	NP_ERR_DLL_NOT_FOUND,
 	NP_ERR_NO_DATA,
-	NP_ERR_INTERNAL_DATA,
+	NP_ERR_INTERNAL_DATA
 
 } NPRESULT;
 
-typedef struct tagTrackIRSignature {
-
+typedef struct tagTrackIRSignature
+{
 	char DllSignature[200];
 	char AppSignature[200];
 
-} SIGNATUREDATA, *LPTRACKIRSIGNATURE;
+} SIGNATUREDATA, * LPTRACKIRSIGNATURE;
 
 typedef struct tagTrackIRData
 {
@@ -133,42 +132,62 @@ typedef struct tagTrackIRData
 	float fNPSmoothY;
 	float fNPSmoothZ;
 
-} TRACKIRDATA, *LPTRACKIRDATA;
+} TRACKIRDATA, * LPTRACKIRDATA;
+
+
+//
+// Typedef for pointer to the notify callback function that is implemented within
+// the client -- this function receives head tracker reports from the game client API
+//
+typedef NPRESULT(__stdcall* PF_NOTIFYCALLBACK)(unsigned short, unsigned short);
 
 // Typedefs for game client API functions (useful for declaring pointers to these
 // functions within the client for use during GetProcAddress() ops)
 //
-// "The syntax for creating a non-const function pointer is one of the ugliest things you will ever see in C++" - Alex @ LearnCPP.com
-typedef NPRESULT (__stdcall *PF_NP_REGISTERWINDOWHANDLE)( HWND );
-typedef NPRESULT (__stdcall *PF_NP_UNREGISTERWINDOWHANDLE)( void );
-typedef NPRESULT (__stdcall *PF_NP_REGISTERPROGRAMPROFILEID)( unsigned short );
-typedef NPRESULT (__stdcall *PF_NP_QUERYVERSION)( unsigned short* );
-typedef NPRESULT (__stdcall *PF_NP_REQUESTDATA)( unsigned short );
-typedef NPRESULT (__stdcall *PF_NP_GETSIGNATURE)( LPTRACKIRSIGNATURE );
-typedef NPRESULT (__stdcall *PF_NP_GETDATA)( LPTRACKIRDATA );
-typedef NPRESULT (__stdcall *PF_NP_STARTCURSOR)( void );
-typedef NPRESULT (__stdcall *PF_NP_STOPCURSOR)( void );
-typedef NPRESULT (__stdcall *PF_NP_RECENTER)( void );
-typedef NPRESULT (__stdcall *PF_NP_STARTDATATRANSMISSION)( void );
-typedef NPRESULT (__stdcall *PF_NP_STOPDATATRANSMISSION)( void );
+typedef NPRESULT(__stdcall* PF_NP_REGISTERWINDOWHANDLE)(HWND);
+typedef NPRESULT(__stdcall* PF_NP_UNREGISTERWINDOWHANDLE)(void);
+typedef NPRESULT(__stdcall* PF_NP_REGISTERPROGRAMPROFILEID)(unsigned short);
+typedef NPRESULT(__stdcall* PF_NP_QUERYVERSION)(unsigned short*);
+typedef NPRESULT(__stdcall* PF_NP_REQUESTDATA)(unsigned short);
+typedef NPRESULT(__stdcall* PF_NP_GETSIGNATURE)(LPTRACKIRSIGNATURE);
+typedef NPRESULT(__stdcall* PF_NP_GETDATA)(LPTRACKIRDATA);
+typedef NPRESULT(__stdcall* PF_NP_REGISTERNOTIFY)(PF_NOTIFYCALLBACK);
+typedef NPRESULT(__stdcall* PF_NP_UNREGISTERNOTIFY)(void);
+typedef NPRESULT(__stdcall* PF_NP_STARTCURSOR)(void);
+typedef NPRESULT(__stdcall* PF_NP_STOPCURSOR)(void);
+typedef NPRESULT(__stdcall* PF_NP_RECENTER)(void);
+typedef NPRESULT(__stdcall* PF_NP_STARTDATATRANSMISSION)(void);
+typedef NPRESULT(__stdcall* PF_NP_STOPDATATRANSMISSION)(void);
 
-#pragma pack( pop, npclient_h ) // Ensure previous pack value is restored
-
-////////////////////////////////////////////////////
-// NaturalPoint Game Client API function wrappers /////////////////////////////
-////////////////////////////////////////////////////
+//// Function Prototypes ///////////////////////////////////////////////
+//
+// Functions exported from game client API DLL ( note __stdcall calling convention
+// is used for ease of interface to clients of differing implementations including
+// C, C++, Pascal (Delphi) and VB. )
+//
 NPRESULT __stdcall NP_RegisterWindowHandle(HWND hWnd);
-NPRESULT __stdcall NP_UnregisterWindowHandle();
+NPRESULT __stdcall NP_UnregisterWindowHandle(void);
 NPRESULT __stdcall NP_RegisterProgramProfileID(unsigned short wPPID);
 NPRESULT __stdcall NP_QueryVersion(unsigned short* pwVersion);
 NPRESULT __stdcall NP_RequestData(unsigned short wDataReq);
 NPRESULT __stdcall NP_GetSignature(LPTRACKIRSIGNATURE pSignature);
 NPRESULT __stdcall NP_GetData(LPTRACKIRDATA pTID);
-NPRESULT __stdcall NP_StartCursor();
-NPRESULT __stdcall NP_StopCursor();
-NPRESULT __stdcall NP_ReCenter();
-NPRESULT __stdcall NP_StartDataTransmission();
-NPRESULT __stdcall NP_StopDataTransmission();
-NPRESULT NPClient_Init(LPTSTR pszDLLPath);
+NPRESULT __stdcall NP_RegisterNotify(PF_NOTIFYCALLBACK pfNotify);
+NPRESULT __stdcall NP_UnregisterNotify(void);
+NPRESULT __stdcall NP_StartCursor(void);
+NPRESULT __stdcall NP_StopCursor(void);
+NPRESULT __stdcall NP_ReCenter(void);
+NPRESULT __stdcall NP_StartDataTransmission(void);
+NPRESULT __stdcall NP_StopDataTransmission(void);
 
-#endif /* NP_CLIENT_H */
+/////////////////////////////////////////////////////////////////////////
+
+#pragma pack( pop, npclient_h ) // Ensure previous pack value is restored
+
+#endif // #ifdef NPCLIENT_H_DEFINED_
+
+//
+// *** End of file: NPClient.h ***
+//
+
+
