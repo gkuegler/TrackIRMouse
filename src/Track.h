b6cbb4d12e669707e1c8a994d4f4c158f6970b57
George Kuegler <georgekuegler@gmail.com>
2021-09-28 16:27:07 -0400

    (class) modify header

--- src/Track.h
+++ src/Track.h
@@ -4,8 +4,19 @@
 
 #include <wx/wx.h>
 #include <windows.h>
+#include "Config.h"
 
 int trackInitialize(wxEvtHandler* m_parent, HWND hWnd);
 int trackStart();
+
+class CTracker
+{
+public:
+    bool m_IsInitialized = false;
+    bool m_IsTracking = false;
+
+    int trackInitialize(wxEvtHandler* m_parent, HWND hWnd, CConfig config);
+    int trackStart(CConfig config);
+}
 
 #endif /* TRACKIRMOUSE_TRACK_H */
