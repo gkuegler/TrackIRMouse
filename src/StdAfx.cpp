786c8b3e131d32ce0f87ff0e73d6d11abbf46ea1
George Kuegler <georgekuegler@gmail.com>
2021-11-14 00:07:59 -0500

    remove unnecessary headers from NPTrackIR sdk

--- src/StdAfx.cpp
+++
@@ -1,6 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	NPTest.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#define _AFXDLL
-#include "stdafx.h"
